284b3b7f9f281d416f7fa411ad60bd7e84851308
Koustav Chowdhury <kc99.kol@gmail.com>
2018-12-23 15:26:25 +0530

    Update ytd01.py

--- lib/ytd01.py
+++ lib/ytd01.py
@@ -4,12 +4,16 @@ import ffmpeg
 import os
 
 def progress(stream = None, chunk = None, file_handle = None, remaining = None):
+
 	percent = (100*(file_size-remaining))/file_size
 	print("{:00.2f} % downloaded ".format(percent) , '{:.2f}'.format((file_size-remaining)/(1024**2)),'MB',' out of ' , '{:.2f}'.format(file_size/(1024**2)),'MB' ,sep="  ")
 
 def yt_downloader():
+	
 	link=input("Enter the video link :: ").strip()
+	
 	print ("Accessing YouTube URL...")
+	
 	try:
 		yt=YouTube(link,on_progress_callback=progress)
 	except:
@@ -17,25 +21,40 @@ def yt_downloader():
 		print('::  Check your connection  ::')
 		print('::  Check the url of the link  ::')
 		exit(0)
+	
 	name=yt.title
 	audio=yt.streams.filter(only_audio=True,type='audio',subtype='mp4').all()
 	sel=1
+	
 	if (len(audio)>1):
 		for audio_t in audio :
 			print ("::::::::::"+str(sel)+"::::::::::")
 			print(audio_t)
 		sel=int(input("??  Enter choice  ??  "))
+	
 	print ("Downloading : {}...".format(name))
 	stream_to_be_downloaded=audio[sel-1]
+	
 	global file_size
 	file_size = stream_to_be_downloaded.filesize
-	print ('File size %.2f MB'.format((file_size)/(1024**2)))
+	print ('File size {:.2f} MB'.format((file_size)/(1024**2)))
+
+	file_path=input('?? Save to ?? [  Enter relative path  ] [  Press ENTER to save in the current directory ] ')
+
+	if (len(file_path)>0):
+		if not os.path.exists(file_path):
+			os.makedirs(file_path)
+			print('Directory', file_path , 'created')
+		else:
+			print('Directory',file_path,'already exists!')
+
+		os.chdir(file_path)
+
 	stream_to_be_downloaded.download(filename='audio')
 	audio_to_be_converted=ffmpeg.input('audio.mp4')
 	ultimate_file=ffmpeg.output(audio_to_be_converted,name,format='mp3')
 	ultimate_file.run()
 	os.remove('audio.mp4')
-
 
 if __name__ == '__main__':
 	yt_downloader()
